cb120f4d6bd5c616a1b57e36f9472c3305e908e6
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-02-08 02:02:08 -0300

    test: listagem transação

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -19,4 +19,31 @@ describe('transactions', () => {
     })
     expect(response.status).toBe(201)
   })
+
+  it('should be able to list all transaction for the user.', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        amount: 100,
+        title: 'test',
+        type: 'credit',
+      })
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    expect(listTransactionsResponse.status).toBe(200)
+    expect(listTransactionsResponse.body.data).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(String),
+          title: 'test',
+          amount: 100,
+          session_id: expect.any(String),
+        }),
+      ]),
+    )
+  })
 })
